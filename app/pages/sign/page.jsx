25566d17adc024d681ee165144e17f66e0a4f90e
kashif <kashif@gmailcom>
2024-07-31 17:55:44 +0500

    final touch 12

--- app/pages/sign/page.jsx
+++ app/pages/sign/page.jsx
@@ -25,17 +25,7 @@ const Sign = () => {
       push("/");
     } else {
       setLoading(false);
-      toast.error("Sorry Sign up with correct credentials", {
-        position: "top-center",
-        autoClose: 5000,
-        hideProgressBar: false,
-        closeOnClick: true,
-        pauseOnHover: true,
-        draggable: true,
-        progress: undefined,
-        theme: "dark",
-        className: "text-sm",
-      });
+      alert("Wrong Credntials");
     }
   };
 
