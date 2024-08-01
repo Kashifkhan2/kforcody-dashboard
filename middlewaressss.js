b835cd2388dd3b807a24affe13796d2495efc0ce
kashif <kashif@gmailcom>
2024-08-01 15:29:13 +0500

    final commit 2

---
+++ middlewaressss.js
@@ -0,0 +1,47 @@
+// export function middleware(request) {
+//   const currentUser = request.cookies.get('authUser')?.value
+//   //ddd
+//   if (!currentUser && request.nextUrl.pathname == "/") {
+//     return Response.redirect(new URL("/pages/sign", request.url));
+//   } else if (
+//     !currentUser &&
+//     request.nextUrl.pathname.startsWith("/pages/addcategory")
+//   ) {
+//     return Response.redirect(new URL("/pages/sign", request.url));
+//   } else if (
+//     !currentUser &&
+//     request.nextUrl.pathname.startsWith("/pages/codeitem")
+//   ) {
+//     return Response.redirect(new URL("/pages/sign", request.url));
+//   } else if (
+//     !currentUser &&
+//     request.nextUrl.pathname.startsWith("/pages/addcodeitem")
+//   ) {
+//     return Response.redirect(new URL("/pages/sign", request.url));
+//   } else if (
+//     !currentUser &&
+//     request.nextUrl.pathname.startsWith("/pages/downloads/")
+//   ) {
+//     return Response.redirect(new URL("/pages/sign", request.url));
+//   } else if (
+//     !currentUser &&
+//     request.nextUrl.pathname.startsWith("/pages/msges")
+//   ) {
+//     return Response.redirect(new URL("/pages/sign", request.url));
+//   } else if (
+//     !currentUser &&
+//     request.nextUrl.pathname.startsWith("/pages/users")
+//   ) {
+//     return Response.redirect(new URL("/pages/sign", request.url));
+//   } else if (
+//     currentUser &&
+//     request.nextUrl.pathname.startsWith("/pages/sign")
+//   ) {
+//     return Response.redirect(new URL("/", request.url));
+//   }
+// }
+
+// // export const config = {
+// //   matcher: "/pages/:path*",
+// //   // matcher: ["/((?!api|_next/static|_next/image|.*\\.png$).*)"],
+// // };
